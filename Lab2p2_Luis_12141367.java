16d72e8ead8a72597223692ec68188bd6a2517b2
luisitomario24 <luisitomario24@yahoo.com>
2022-01-28 14:45:01 -0600

    metodo registro

--- Lab2p2_Luis_12141367.java
+++ Lab2p2_Luis_12141367.java
@@ -18,14 +18,13 @@ public class Lab2p2_Luis_12141367 {
         System.out.println("");
         System.out.println("");
         System.out.println("0. Salir");
-        System.out.println("1.Editar nombre cientifico\n" +
+        System.out.println("Registrar animal" +
+                "1.Editar nombre cientifico\n" +
                 "2.Editar todos los atributos del animal\n" +
                 "3. Imprimir por posicion de la lista\n" +
                 "4. Impirmir lista completa\n" +
                 "5. Imprimir por nombre cientifico\n" +
                 "6. Eliminar animal\n" +
-                "7. Mi perfil\n" +
-                "8. Administrador Random\n" +
                 "Ingrese la opcion: ");
         return leer.nextInt();
     }
@@ -33,7 +32,7 @@ public class Lab2p2_Luis_12141367 {
         if(opcion == 0)
             System.exit(0);
         else if(opcion == 1){
-
+            registarAnimales();
         }
 
         /*else if(opcion == 2)
@@ -46,5 +45,22 @@ public class Lab2p2_Luis_12141367 {
 
         else if(opcion == 6)
           */
+    }
+    public static void registarAnimales(){
+        System.out.print("Nombre cientifico: ");
+        String nombreCientifico = leer.nextLine();
+        System.out.print("Nombre comun: ");
+        String nombreComun = leer.next();
+        System.out.print("Habitat: ");
+        String Habitat = leer.next();
+        System.out.print("Alimentacion: ");
+        String alimentacion = leer.next();
+        System.out.print("Rasgos: ");
+        String rasgos = leer.nextLine();
+        System.out.print("Distribucion: ");
+        String Distribucion = leer.nextLine();
+        System.out.print("Años de vida: ");
+        int vida = leer.nextInt();
+        animales.add(new Animal(nombreCientifico, nombreComun, Habitat, alimentacion, rasgos, Distribucion,vida));
     }
 }
