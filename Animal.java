da1d11118e93fb23d7592be9940aad9dac071183
luisitomario24 <luisitomario24@yahoo.com>
2022-01-28 13:49:26 -0600

    subiendo proyecto

---
+++ Animal.java
@@ -0,0 +1,97 @@
+package Lab_Progra2;
+
+import java.util.Scanner;
+
+public class Animal {
+    Scanner leer = new Scanner(System.in);
+    private String nombreCientifico;
+    private String nombreComun;
+    private String Habitat;
+    private String alimentacion;
+    private String rasgos;
+    private String Distribucion;
+    private int vida;
+
+    public Animal(){
+
+    }
+
+    public Animal(String nombreCientifico,String nombreComun,String Habitat,String alimentacion,String rasgos,String Distribucion,int vida) {
+        this.nombreCientifico = nombreCientifico;
+        this.nombreComun = nombreComun;
+        this.Habitat=Habitat;
+        this.alimentacion=alimentacion;
+        this.rasgos=rasgos;
+        this.Distribucion=Distribucion;
+        this.vida=vida;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public void setHabitat(String habitat) {
+        Habitat = habitat;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public void setRasgos(String rasgos) {
+        this.rasgos = rasgos;
+    }
+
+    public void setDistribucion(String distribucion) {
+        Distribucion = distribucion;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    public String getHabitat() {
+        return Habitat;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public String getRasgos() {
+        return rasgos;
+    }
+
+    public String getDistribucion() {
+        return Distribucion;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    @Override
+    public String toString() {
+        return "leer=" + leer +
+                ", nombreCientifico='" + nombreCientifico + '\'' +
+                ", nombreComun='" + nombreComun + '\'' +
+                ", Habitat='" + Habitat + '\'' +
+                ", alimentacion='" + alimentacion + '\'' +
+                ", rasgos='" + rasgos + '\'' +
+                ", Distribucion='" + Distribucion + '\'' +
+                ", vida=" + vida +
+                '}';
+    }
+}
---
+++ Lab2p2_Luis_12141367.java
@@ -0,0 +1,17 @@
+package Lab_Progra2;
+
+import java.util.Scanner;
+
+public class Lab2p2_Luis_12141367 {
+
+
+    Scanner leer = new Scanner(System.in);
+    private String nombreCientifico;
+    private String nombreComun;
+    private String Habitat;
+    private String alimentacion;
+
+    public static void main(String[] args) {
+
+    }
+}
